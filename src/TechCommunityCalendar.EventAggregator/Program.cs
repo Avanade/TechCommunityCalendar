bfe6997171d3ad3e8c1fb7bdb25c7d0aa44be85f
Lee Englestone <lee.englestone@gmail.com>
2022-09-16 16:31:52 +0100

    Moving non web files out - (I broke the site)

---
+++ src/TechCommunityCalendar.EventAggregator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TechCommunityCalendar.EventAggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // https://www.techevents.online/
+        }
+    }
+}
