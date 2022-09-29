ebf5409d5ce7fb9a53c1196074657187abe657b3
Lee Englestone <lee.englestone@gmail.com>
2022-09-29 10:23:13 +0100

    Revert "Adding security headers requested by Accenture"

--- src/TechCommunityCalendar.Solution/TechCommunityCalendar.CoreWebApplication/Startup.cs
+++ src/TechCommunityCalendar.Solution/TechCommunityCalendar.CoreWebApplication/Startup.cs
@@ -72,25 +72,12 @@ namespace TechCommunityCalendar.CoreWebApplication
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-
-                app.Use(async (context, next) =>
-                {
-                    // Accenture required headings
-                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubdomains");
-                    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                    context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
-
-                    context.Response.Headers.Add("X-Frame-Options", "DENY");
-                    context.Response.Headers.Add("Cache-Control", "public, max-age=31536000");
-                    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self' 'unsafe-eval' 'unsafe-inline' *.techcommunitycalendar.com; upgrade-insecure-requests; block-all-mixed-content");
-
-                    await next();
-                });
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
             app.UseRouting();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
